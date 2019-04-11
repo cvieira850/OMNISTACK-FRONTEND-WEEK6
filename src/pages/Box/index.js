9e21716606dc13a2925fec4d9db7a2c284e7a0d3
caio vieira <caiodmvieiragithub@gmail.com>
2019-04-10 23:45:46 -0300

    ajustando layout da pagina box , usando date-fns para ajustar o tempode inserção e socket.io-client que é uma lib para trabalhar junto do socket que funciona no backend

--- src/pages/Box/index.js
+++ src/pages/Box/index.js
@@ -1,4 +1,9 @@
 import React, { Component } from "react";
+import api from "../../services/api";
+import { distanceInWords } from "date-fns";
+import pt from "date-fns/locale/pt";
+import Dropzone from "react-dropzone";
+import socket from "socket.io-client";
 
 import { MdInsertDriveFile } from "react-icons/md";
 
@@ -6,31 +11,67 @@ import logo from "../../assets/logo.svg";
 import "./styles.css";
 
 export default class Box extends Component {
+  state = { box: {} };
+
+  async componentDidMount() {
+    this.subscribeTonNewFiles();
+
+    const box = this.props.match.params.id;
+    const response = await api.get(`boxes/${box}`);
+
+    this.setState({ box: response.data });
+  }
+  subscribeTonNewFiles = () => {
+    const box = this.props.match.params.id;
+    const io = socket("https://dropbox-omnistack.herokuapp.com");
+
+    io.emit("connectRoom", box);
+  };
+  handleUpload = files => {
+    files.forEach(file => {
+      const data = new FormData();
+      const box = this.props.match.params.id;
+      data.append("file", file);
+
+      api.post(`boxes/${box}/files`, data);
+    });
+  };
+
   render() {
     return (
       <div id="box-container">
         <header>
           <img src={logo} alt="" />
-          <h1>Teste</h1>
+          <h1>{this.state.box.title}</h1>
         </header>
 
+        <Dropzone onDropAccepted={this.handleUpload}>
+          {({ getRootProps, getInputProps }) => (
+            <div className="upload" {...getRootProps()}>
+              <input {...getInputProps()} />
+
+              <p> Arraste arquivos ou clique aqui</p>
+            </div>
+          )}
+        </Dropzone>
+
         <ul>
-          <li>
-            <a href="">
-              <MdInsertDriveFile size={24} color="#A5Cfff" />
-              <strong>Desafio.pdf</strong>
-            </a>
+          {this.state.box.files &&
+            this.state.box.files.map(file => (
+              <li key={file._id}>
+                <a className="fileInfo" href={file.url} target="_blank">
+                  <MdInsertDriveFile size={24} color="#A5Cfff" />
+                  <strong>{file.title}</strong>
+                </a>
 
-            <span> Há 3 minutos atrás </span>
-          </li>
-          <li>
-            <a href="">
-              <MdInsertDriveFile size={24} color="#A5Cfff" />
-              <strong>Desafio.pdf</strong>
-            </a>
-
-            <span> Há 3 minutos atrás </span>
-          </li>
+                <span>
+                  há{" "}
+                  {distanceInWords(file.createdAt, new Date(), {
+                    locale: pt
+                  })}
+                </span>
+              </li>
+            ))}
         </ul>
       </div>
     );
